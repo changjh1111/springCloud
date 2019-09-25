e0659eda38532d69e917cf5174c31a49e1b5bb74
chang <changjh1111@163.com>
2019-09-25 11:54:43 +0800

    ceshi

--- service_ribbon/src/main/java/com/chang/controller/HelloController.java
+++ service_ribbon/src/main/java/com/chang/controller/HelloController.java
@@ -1,10 +1,18 @@
 package com.chang.controller;
 
 import com.chang.service.HelloService;
+import com.crossoverjie.distributed.annotation.CommonLimit;
+import com.crossoverjie.distributed.annotation.ControllerLimit;
+import com.crossoverjie.distributed.limit.RedisLimit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @Author chang
@@ -12,13 +20,36 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description
  */
 @RestController
+//@ComponentScan(value = "com.crossoverjie.distributed.intercept")
 public class HelloController {
+
+    private int a = 0;
 
     @Autowired
     HelloService helloService;
 
+    @Resource
+    RedisLimit redisLimit;
+
     @GetMapping(value = "/hi")
+    @CommonLimit
     public String getData (@RequestParam String name) {
-        return helloService.getData(name);
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        System.out.println(redisLimit.limit());
+
+
+//        for (int i = 0; i < 300; i++) {
+//            cachedThreadPool.execute(new Runnable() {
+//                @Override
+//                public void run() {
+//                    helloService.getData(name);
+//                    System.out.println(Thread.currentThread());
+//                }
+//            });
+//        }
+//        a += 1;
+//        System.out.println(a);
+        System.out.println(System.currentTimeMillis());
+        return null;
     }
 }
